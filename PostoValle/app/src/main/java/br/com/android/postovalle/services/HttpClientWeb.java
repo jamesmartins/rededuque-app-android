3b03be016bc1c56134305d2b03786b4adc827eaf
James Martins <james.mendes@gmail.com>
2024-02-19 16:15:07 -0300

    Added Postos Valle and RelBio Folders

---
+++ PostoValle/app/src/main/java/br/com/android/postovalle/services/HttpClientWeb.java
@@ -0,0 +1,41 @@
+package br.com.android.postovalle.services;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+/**
+ * Created by macjames on 04/05/16.
+ */
+
+public class HttpClientWeb {
+    private OkHttpClient client = new OkHttpClient();
+
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    public String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+}
