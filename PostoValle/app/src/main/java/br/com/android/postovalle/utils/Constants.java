817cb16a238402c2ed79eac5fb30188e0292b2c9
James Martins <james.mendes@gmail.com>
2024-02-19 17:25:30 -0300

    Update migration

--- PostoValle/app/src/main/java/br/com/android/postovalle/utils/Constants.java
+++ PostoValle/app/src/main/java/br/com/android/postovalle/utils/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
     public static final int FASTEST_LOCATION_INTERVAL = 5000;
 
     public static final String mUrl = "https://adm.bunker.mk/app/intro.do?key=Ms%C2%A3KMIDVxjfNMbzY20SH4LsnjBwg8QB80yME7pKkfoEo%C2%A2";
-    public static final String mUrlNew = "https://adm.bunkerapp.com.br/app/intro.do?key=Ms%C2%A3KMIDVxjfNMbzY20SH4LsnjBwg8QB80yME7pKkfoEo%C2%A2"
-//    public static final String mUrl = "http://adm.bunker.mk/app/intro.do?key=jsop%C2%A369XkOztMd8KYuTjqkQ%C2%A2%C2%A2";
+    public static final String mUrlNew = "https://adm.bunkerapp.com.br/app/intro.do?key=Ms%C2%A3KMIDVxjfNMbzY20SH4LsnjBwg8QB80yME7pKkfoEo%C2%A2";
+                                         "https://adm.bunkerapp.com.br/app/intro.do?key=Ys%C2%A3p%C2%A3u6k50jp%C2%A3M%C2%A2"
 
     public static final String mUrlDev = "https://www.rededuque.com.br/app/";
     public static final String mUrlDev4 = "http://bunker.mk/appduque/home.php";
