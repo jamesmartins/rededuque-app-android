fc407e2bdbdf88c46e27846a8e2199fd1f8e4582
James Martins <james.mendes@gmail.com>
2024-02-25 12:53:35 -0300

    Clean files RedeDuque 4 base code.
    Removed all files RedeDuque 4 base code
    1 splash view
    1 main activity
    1 webview logic component for all views web app

--- RedeDuqueApp4/app/src/main/java/br/com/rededuque/android/model/UrlServer.kt
+++
@@ -1,6 +0,0 @@
-package br.com.rededuque.android.model
-
-class UrlServer(var urlDefault: String? = null,
-                var url2: String? = null,
-                var url3: String? = null,
-                var url4: String? = null )
