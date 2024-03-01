79303d85082883465974979346c193687ce52df6
James Martins <james.mendes@gmail.com>
2024-03-01 06:36:24 -0300

    Update new style and rules for login

--- RedeDuqueApp5/app/src/main/java/br/com/rededuque/android/LoginActivity2.kt
+++ RedeDuqueApp5/app/src/main/java/br/com/rededuque/android/LoginActivity2.kt
@@ -2,11 +2,31 @@ package br.com.rededuque.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.lifecycle.LiveData
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity2 : AppCompatActivity() {
+
+    var btnLogin: Button? = null
+    var editLogin: TextInputEditText? = null
+    var editPasswd: TextInputEditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+
+        initViews()
+    }
+
+    fun initViews(){
+        var btnManterDadosLogin = findViewById<SwitchMaterial>(R.id.txtCheckLogin)
+        btnManterDadosLogin.isChecked = true
+        editLogin = findViewById<TextInputEditText>(R.id.edtLogin)
+        editPasswd = findViewById<TextInputEditText>(R.id.edtPasssword)
+
     }
 
     override fun onBackPressed() {
